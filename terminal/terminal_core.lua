efd41695692ecb79de61fbe9a11b3d539686810c
Benjamin Lister <48570319+007spy9@users.noreply.github.com>
2023-08-18 17:17:26 +0100

    Folder Structure

---
+++ terminal/terminal_core.lua
@@ -0,0 +1,9 @@
+--Code will be deployed in ComputerCraft
+
+--Load the cryptoNet API
+os.loadAPI("cryptoNET")
+
+--region Constants
+--The server address for the host controller
+CLIENT_ACCESS_SERVER = "BankNet.BankSys.Client"
+--endregion
